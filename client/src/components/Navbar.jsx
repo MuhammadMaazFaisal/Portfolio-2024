1122ef735557fecb6e4d32bd925a01affbf4bd48
Muhammad Maaz Faisal <m.maazfaisal0301@gmail.com>
2024-10-31 23:01:12 +0500

    Feat: Update Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "preline";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
@@ -26,75 +27,101 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav
-      className={`${
-        styles.paddingX
-      } w-full flex items-center py-5 fixed top-0 z-20 ${
-        scrolled ? "bg-primary" : "bg-transparent"
-      }`}
-    >
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link
-          to='/'
-          className='flex items-center gap-2'
-          onClick={() => {
-            setActive("");
-            window.scrollTo(0, 0);
-          }}
-        >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex ps-4'>
-           Muhammad  Maaz
-          </p>
-        </Link>
-
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {navLinks.map((nav) => (
-            <li
-              key={nav.id}
-              className={`${
-                active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(nav.title)}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
-        </ul>
-
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <img
-            src={toggle ? close : menu}
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain'
-            onClick={() => setToggle(!toggle)}
-          />
-
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
-              {navLinks.map((nav) => (
-                <li
-                  key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-secondary"
-                  }`}
+    <>
+      <header class="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+      <nav class="mt-4 relative max-w-5xl w-full bg-white/10 backdrop-blur-md border border-gray-200 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-800/20 dark:border-neutral-700">
+      <div class="px-4 md:px-0 flex justify-between items-center">
+            <div>
+ 
+                <Link
+                  to="/"
+                  className="flex items-center gap-2"
                   onClick={() => {
-                    setToggle(!toggle);
-                    setActive(nav.title);
+                    setActive("");
+                    window.scrollTo(0, 0);
                   }}
                 >
-                  <a href={`#${nav.id}`}>{nav.title}</a>
-                </li>
-              ))}
-            </ul>
+                  <img
+                    src={logo}
+                    alt="logo"
+                    className="w-9 h-9 object-contain"
+                  />
+                  {/* <p className="text-white text-[18px] font-bold cursor-pointer flex ps-4 text-nowrap">
+                    Muhammad Maaz
+                  </p> */}
+                </Link>
+            </div>
+
+            <div class="md:hidden">
+              <button
+                type="button"
+                class="hs-collapse-toggle flex justify-center items-center size-6 border border-gray-200 text-gray-500 rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                id="hs-navbar-header-floating-collapse"
+                aria-expanded="false"
+                aria-controls="hs-navbar-header-floating"
+                aria-label="Toggle navigation"
+                data-hs-collapse="#hs-navbar-header-floating"
+              >
+                <svg
+                  class="hs-collapse-open:hidden shrink-0 size-3.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <line x1="3" x2="21" y1="6" y2="6" />
+                  <line x1="3" x2="21" y1="12" y2="12" />
+                  <line x1="3" x2="21" y1="18" y2="18" />
+                </svg>
+                <svg
+                  class="hs-collapse-open:block hidden shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M18 6 6 18" />
+                  <path d="m6 6 12 12" />
+                </svg>
+              </button>
+            </div>
           </div>
-        </div>
-      </div>
-    </nav>
+
+          <div
+            id="hs-navbar-header-floating"
+            class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block"
+            aria-labelledby="hs-navbar-header-floating-collapse"
+          >
+            <div class="flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
+              {navLinks.map((nav) => (
+                <a
+                  key={nav.id}
+                  className={`${
+                    active === nav.title
+                      ? "border-gray-800 font-medium text-gray-800 focus:outline-none dark:border-neutral-200 dark:text-white"
+                      : "border-transparent text-gray-500 hover:text-gray-800 focus:outline-none dark:text-neutral-200 dark:hover:text-white"
+                  } py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 text-[16px]`}
+                  href={`#${nav.id}`}
+                  onClick={() => setActive(nav.title)}
+                >
+                  {nav.title}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 };
 
