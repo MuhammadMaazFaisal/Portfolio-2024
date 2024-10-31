1acbcce18d68beb19cc377429285c780bc44f5f0
Muhammad Maaz Faisal <m.maazfaisal0301@gmail.com>
2024-11-01 01:59:43 +0500

    Feat: Added modal for Projects

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,9 +28,9 @@ const Navbar = () => {
 
   return (
     <>
-      <header class="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
-        <nav class="mt-4 relative max-w-5xl w-[90%] mx-auto bg-white/10 backdrop-blur-md border border-gray-200 rounded-[2rem] py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-800/20 dark:border-neutral-700">
-          <div class="px-4 md:px-0 flex justify-between items-center">
+      <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+        <nav className="mt-4 relative max-w-5xl w-[90%] mx-auto bg-white/10 backdrop-blur-md border border-gray-200 rounded-[2rem] py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-800/20 dark:border-neutral-700">
+          <div className="px-4 md:px-0 flex justify-between items-center">
             <div>
               <Link
                 to="/"
@@ -47,10 +47,10 @@ const Navbar = () => {
               </Link>
             </div>
 
-            <div class="md:hidden">
+            <div className="md:hidden">
               <button
                 type="button"
-                class="hs-collapse-toggle flex justify-center items-center size-6 border border-gray-200 text-gray-500 rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                className="hs-collapse-toggle flex justify-center items-center size-6 border border-gray-200 text-gray-500 rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
                 id="hs-navbar-header-floating-collapse"
                 aria-expanded="false"
                 aria-controls="hs-navbar-header-floating"
@@ -58,32 +58,32 @@ const Navbar = () => {
                 data-hs-collapse="#hs-navbar-header-floating"
               >
                 <svg
-                  class="hs-collapse-open:hidden shrink-0 size-3.5"
+                  className="hs-collapse-open:hidden shrink-0 size-3.5"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <line x1="3" x2="21" y1="6" y2="6" />
                   <line x1="3" x2="21" y1="12" y2="12" />
                   <line x1="3" x2="21" y1="18" y2="18" />
                 </svg>
                 <svg
-                  class="hs-collapse-open:block hidden shrink-0 size-4"
+                  className="hs-collapse-open:block hidden shrink-0 size-4"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <path d="M18 6 6 18" />
                   <path d="m6 6 12 12" />
@@ -94,10 +94,10 @@ const Navbar = () => {
 
           <div
             id="hs-navbar-header-floating"
-            class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block"
+            className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block"
             aria-labelledby="hs-navbar-header-floating-collapse"
           >
-            <div class="flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
+            <div className="flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
               {navLinks.map((nav) => (
                 <a
                   key={nav.id}
--- client/src/components/Works.jsx
+++ client/src/components/Works.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
+import { fadeIn, textVariant, zoomIn } from "../utils/motion";
+import "preline"; // Make sure Preline.js is imported
 
 const ProjectCard = ({
   index,
@@ -15,48 +15,57 @@ const ProjectCard = ({
   tags,
   video,
   source_code_link,
+  onClick,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div
+      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+      onClick={onClick}
+    >
       <Tilt
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full cursor-pointer"
       >
-        <div className='relative w-full h-[230px]'>
+        <div className="relative w-full h-[230px]">
           <video
             autoPlay
             loop
             muted
             playsInline
             src={video}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
+            alt="project_video"
+            className="w-full h-full object-cover rounded-2xl"
           />
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              onClick={(e) => {
+                e.stopPropagation();
+                window.open(source_code_link, "_blank");
+              }}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
                 src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
               />
             </div>
           </div>
         </div>
 
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[22px] text-center'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px] text-center'>{intro}</p>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[22px] text-center">
+            {name}
+          </h3>
+          <p className="mt-2 text-secondary text-[14px] text-center">{intro}</p>
         </div>
 
-        <div className='mt-4 flex flex-wrap gap-2 justify-center'>
+        <div className="mt-4 flex flex-wrap gap-2 justify-center">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
@@ -72,17 +81,29 @@ const ProjectCard = ({
 };
 
 const Works = () => {
+  const [modalData, setModalData] = useState(null);
+
+  const openModal = (project) => {
+    setModalData(project);
+    document.body.style.overflow = "hidden"; // Disable background scrolling
+  };
+
+  const closeModal = () => {
+    setModalData(null);
+    document.body.style.overflow = "auto"; // Enable background scrolling again
+  };
+
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
+        <p className={`${styles.sectionSubText}`}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
@@ -92,13 +113,101 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+            onClick={() => openModal(project)}
+          />
         ))}
       </div>
+
+      {/* Modal rendering based on state */}
+      {modalData && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-60 backdrop-blur-sm flex items-center justify-center z-[100] overflow-y-auto"
+          role="dialog"
+          aria-labelledby="project-modal-label"
+        >
+          <motion.div
+            variants={zoomIn(0.1, 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="bg-white shadow-2xl rounded-3xl dark:bg-primary/75 backdrop-blur max-w-5xl w-full mx-4 md:mx-8 p-6 md:p-10 relative sm:mt-0 mt-[20vh]"
+          >
+            <button
+              type="button"
+              className="absolute top-4 right-4 md:top-6 md:right-6 inline-flex justify-center items-center size-10 text-lg font-semibold rounded-full border border-transparent bg-white/20 text-white hover:bg-white/30 dark:bg-neutral-700 dark:hover:bg-neutral-600 z-[150]"
+              aria-label="Close"
+              onClick={closeModal}
+            >
+              <span className="sr-only">Close</span>
+              <svg
+                className="shrink-0 size-6"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="M6 6 18 18" />
+              </svg>
+            </button>
+
+            <div className="flex flex-col lg:flex-row items-center lg:items-center gap-8">
+              <div className="lg:w-1/2 w-full flex flex-col items-center lg:items-center justify-center">
+                <video
+                  controls
+                  src={modalData.video}
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  alt="project_video"
+                  controlsList="nodownload"
+                  className="w-full rounded-lg shadow-lg mb-6 lg:mb-4"
+                ></video>
+                <div className="flex flex-wrap gap-3 mb-6 lg:mb-0 justify-center">
+                  {modalData.tags.map((tag) => (
+                    <span
+                      key={tag.name}
+                      className={`px-3 py-1.5 text-sm font-medium rounded-full ${tag.color} bg-opacity-10 border border-current text-center`}
+                    >
+                      #{tag.name}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="lg:w-1/2 w-full flex flex-col overflow-y-auto max-h-[60vh]">
+                <h3
+                  id="project-modal-label"
+                  className="mb-4 text-4xl font-bold text-gray-900 dark:text-neutral-100"
+                >
+                  {modalData.name}
+                </h3>
+                <p className="text-gray-600 dark:text-neutral-400 mb-4 leading-relaxed text-justify pe-3">
+                  {modalData.intro}
+                </p>
+                <p className="text-gray-600 dark:text-neutral-400 mb-6 leading-relaxed text-justify pe-3">
+                  {modalData.details.description}
+                </p>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      )}
     </>
   );
 };
+
+
 
 export default SectionWrapper(Works, "projects");
